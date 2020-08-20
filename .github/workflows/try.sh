15e02ff1fbb9a91a643e492b44c905363384850a
Xintao <wxt1994@126.com>
2020-08-20 20:48:15 +0800

    update

--- .github/workflows/try.sh
+++ .github/workflows/try.sh
@@ -4,13 +4,12 @@ pwd
 
 
 
-if [[ -n "$SSH_PRIVATE_KEY" ]]
-then
-    echo 'yyyyyyyyyyyyyyyy'
-    mkdir -p /root/.ssh
-    echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
-    chmod 600 /root/.ssh/id_rsa
-fi
+
+echo 'yyyyyyyyyyyyyyyy'
+mkdir -p /root/.ssh
+echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
 
 echo 'xxxxxxxxx'
 
